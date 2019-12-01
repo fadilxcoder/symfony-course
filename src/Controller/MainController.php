e26c08f211245f50621693d252ff439bbd6645ee
Fadil <rosunmungurudm@gmail.com>
2019-12-01 16:36:16 +0400

    sub-request - twig & controller & base

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -33,4 +33,27 @@ class MainController extends AbstractController
             'name'      => $getName
         ]);
     }
+
+    // only returns a fragment of HTML
+    public function _makeTweets($max)
+    {
+        $tweets = [
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas cursus mi at feugiat consectetur',
+            'In tristique vel quam quis facilisis. Donec vel dui ac sem porttitor iaculis blandit sed odio.',
+            'Ut mattis aliquam feugiat. Praesent libero massa, facilisis at enim ut, pretium feugiat nunc. Praesent finibus nulla id dui cursus, ac varius velit luctus.',
+            'Praesent gravida elementum ex, nec rhoncus arcu tempor nec.',
+            'Phasellus iaculis nibh fringilla justo gravida pharetra. Nullam congue pulvinar odio.',
+            'Nunc pellentesque aliquam malesuada. Curabitur faucibus felis id odio pharetra, eu hendrerit metus malesuada.',
+            'Ut mi est, sodales non congue eu, vehicula id nisl. Ut sed convallis ipsum.',
+            'Donec a sapien vitae lacus varius pharetra eget in velit.',
+            'Maecenas ullamcorper orci sit amet ultrices ornare.'
+        ];
+        shuffle($tweets);
+        $randomTweets = array_slice($tweets, 0, $max);
+
+        return $this->render('includes/_tweets.html.twig', [
+            'name'      => 'Some fake tweets',
+            'tweets'    => $randomTweets
+        ]);
+    }
 }
